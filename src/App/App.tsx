0cb22d3f7633379ca87e34530c90985a71bd1f6f
Preeti <>
2018-11-21 16:27:24 +0530

    Added Font Awesome and replaced default image

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
 import Home from './screens/Home';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faUserCircle);
 
 class App extends React.Component {
   public render() {
--- src/App/components/BasicInfo/BasicInfo.tsx
+++ src/App/components/BasicInfo/BasicInfo.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './BasicInfo.css';
-import logo from '../../../logo.svg';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export interface Props {
     name: string;
@@ -8,12 +9,14 @@ export interface Props {
     profileImg?: string;
 }
 
-
-function BasicInfo({ name, designation, profileImg = logo }: Props) {
-    
+function BasicInfo({ name, designation, profileImg = '' }: Props) {
     return(
         <>
-            <img src={profileImg} className="profile-image" />
+            {   
+                (profileImg.length !== 0) ?
+                <img src={profileImg} className="profile-image" /> :
+                <FontAwesomeIcon icon="user-circle" size="3x" className="default-image" />
+            }
             <div className="text-large">{name}</div>
             <div className="text-medium">{designation}</div>
         </>
